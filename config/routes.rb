eafa1325bf4712f9b391bfb54ed875ea2af83324
Duc <trinhquangminh2003.27@gmai.com>
2023-11-24 00:28:00 +0700

    homepage

--- config/routes.rb
+++ config/routes.rb
@@ -18,5 +18,6 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root  "library_system_management#home"
+  root to:'mingg#home'
 end
+
